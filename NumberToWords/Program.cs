f6a86a474fbcb640ec8cedec69d9b94140035594
Yusuf Naheem <yusufnaheem1@gmail.com>
2021-08-24 15:22:32 +0100

    Initial Commit

---
+++ NumberToWords/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberToWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Please enter a number you would like to see converted into the written format below.
+            Console.WriteLine(NumberToWords(401062));
+            Console.ReadLine();
+        }
+
+        public static string NumberToWords(int number)
+        {
+            if (number == 0)
+                return "zero";
+
+            if (number < 0)
+                return "minus " + NumberToWords(Math.Abs(number));
+
+            string words = "";
+
+            if ((number / 1000000) > 0)
+            {
+                words += NumberToWords(number / 1000000) + " million ";
+                number %= 1000000;
+            }
+
+            if ((number / 1000) > 0)
+            {
+                words += NumberToWords(number / 1000) + " thousand ";
+                number %= 1000;
+            }
+
+            if ((number / 100) > 0)
+            {
+                words += NumberToWords(number / 100) + " hundred ";
+                number %= 100;
+            }
+
+            if (number > 0)
+            {
+                if (words != "")
+                    words += "and ";
+
+                var units = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+
+                var tens = new[] { "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
+                if (number < 20)
+                    words += units[number];
+                else
+                {
+                    words += tens[number / 10];
+                    if ((number % 10) > 0)
+                        words += "-" + units[number % 10];
+                }
+            }
+
+            return words;
+        }
+    }
+}
